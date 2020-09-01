af5c909e556b21c9c3b0dcdd0d9c6036f691893b
SHPSWP <70546931+SHPSWP@users.noreply.github.com>
2020-09-01 12:38:40 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,81 @@
+function makemap () {
+    tiles.setTilemap(tiles.createTilemap(hex`200020000202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020206060607020202020202020202020202020203030303030303030303030303030101010d020202020202020202020202020203030303030303030303030303030101010d02020202020202020202020202020202020202020202020202020202130b010d020202020202020202020202020202020202020202020202020202020212141102020202020202020202020202020202020202020202020202020202021214110202020202020202020202020202020202020202050f060f0702020202121711020506070202020202020202020202020202020509031a030c070202021214110509190c0702020202020202020202020202021203030303030c06060609140c091903180c07020202020202020202020202021003030303031414141714031718031903190d020202020202020202020202021203030303030a1313130b140a0b1903190a08020202020202020202020202020e0b0303030a0801010110170d0e0b190a080202020202020202020202020202020e0b130a080101020110140d010e1308020202020202020202020202020202020210030d010101010509140c07010202020202020202020202020202020202020210030d011c0105090303030c070202020202020202020202020202020202020210030d0101050903031403030c0702050407020202020202020202020202020210140d010110030303160303030c06091b0c070202020202020202020202020210030d010110031716141614031b1b1b1b1b150202020202020202020202020210140c060609030303160303030a130b1b0a08020202020202020202020202021003141414031403031403030a09020e130802020202020202020202020202020e1313131313140b0303030a0902020202020202020202020202020202020202020202020202020e1313130902020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`, img`
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . 2 2 2 2 2 . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . 2 2 . 2 . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . 2 . 2 . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . 2 . 2 . . . . . . . . . . . . . . 
+        . . . . . . 2 2 2 2 2 . . . . 2 . 2 . 2 2 2 . . . . . . . . . . 
+        . . . . . 2 2 . . . 2 2 . . . 2 . 2 2 2 . 2 2 . . . . . . . . . 
+        . . . . . 2 . . . . . 2 2 2 2 2 . 2 2 . . . 2 2 . . . . . . . . 
+        . . . . . 2 . . . . . . . . . . . . . . . . . 2 . . . . . . . . 
+        . . . . . 2 . . . . . 2 2 2 2 2 . 2 2 . . . 2 2 . . . . . . . . 
+        . . . . . 2 2 . . . 2 2 . . . 2 . 2 2 2 . 2 2 . . . . . . . . . 
+        . . . . . . 2 2 2 2 2 . . . . 2 . 2 . 2 2 2 . . . . . . . . . . 
+        . . . . . . . 2 . 2 . . . . 2 2 . 2 2 . . . . . . . . . . . . . 
+        . . . . . . . 2 . 2 . . . 2 2 . . . 2 2 . . . . . . . . . . . . 
+        . . . . . . . 2 . 2 . . 2 2 . . . . . 2 2 . 2 2 2 . . . . . . . 
+        . . . . . . . 2 . 2 . . 2 . . . . . . . 2 2 2 . 2 2 . . . . . . 
+        . . . . . . . 2 . 2 . . 2 . . . . . . . . . . . . 2 . . . . . . 
+        . . . . . . . 2 . 2 2 2 2 . . . . . . . 2 2 2 . 2 2 . . . . . . 
+        . . . . . . . 2 . . . . . . . . . . . 2 2 2 2 2 2 2 . . . . . . 
+        . . . . . . . 2 2 2 2 2 2 . 2 . . . 2 2 . . . . . 2 . . . . . . 
+        . . . . . . . . . . . . . . 2 2 2 2 2 . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+        `, [myTiles.transparency16,sprites.dungeon.hazardLava0,sprites.dungeon.hazardLava1,sprites.dungeon.floorDarkDiamond,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterSouth0,sprites.dungeon.floorDark2,sprites.dungeon.greenOuterEast0,sprites.dungeon.floorDark0,sprites.dungeon.floorDark5,sprites.dungeon.floorDark3,sprites.dungeon.floorDark1,sprites.dungeon.greenSwitchUp,sprites.dungeon.floorLight2,sprites.dungeon.darkGroundCenter], TileScale.Sixteen))
+}
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenSwitchUp, function (sprite, location) {
+    tiles.setTileAt(tiles.getTileLocation(8, 11), sprites.dungeon.floorDarkDiamond)
+})
+let mySprite = sprites.create(img`
+    . . . f f f f f . . . . . 
+    . f f f f f f f f f . . . 
+    . f f f f f f c f f f . . 
+    f f f f c f f f c f f . . 
+    f c f f c c f f f c c f f 
+    f c c f f f f e f f f f f 
+    f f f f f f f e e f f f . 
+    f f e e f b f e e f f . . 
+    . f e 4 e 1 f 4 4 f . . . 
+    . f f f e 4 4 4 4 f . . . 
+    . . f e e e e e f f . . . 
+    . . e 4 4 e 7 7 7 f . . . 
+    . . e 4 4 e 7 7 7 f . . . 
+    . . f e e f 6 6 6 f . . . 
+    . . . f f f f f f . . . . 
+    . . . . f f f . . . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite)
+makemap()
+let mySprite2 = sprites.create(img`
+    . . . c c c c c c . . . . . . . 
+    . . c 6 7 7 7 7 6 c . . . . . . 
+    . c 7 7 7 7 7 7 7 7 c . . . . . 
+    c 6 7 7 7 7 7 7 7 7 6 c . . . . 
+    c 7 c 6 6 6 6 c 7 7 7 c . . . . 
+    f 7 6 f 6 6 f 6 7 7 7 f . . . . 
+    f 7 7 7 7 7 7 7 7 7 7 f . . . . 
+    . f 7 7 7 7 6 c 7 7 6 f . . . . 
+    . . f c c c c 7 7 6 f c c c . . 
+    . . c 6 2 7 7 7 f c c 7 7 7 c . 
+    . c 6 7 7 2 7 7 c f 6 7 7 7 7 c 
+    . c 1 1 1 1 7 6 6 c 6 6 6 c c c 
+    . c 1 1 1 1 1 6 6 6 6 6 6 c . . 
+    . c 6 1 1 1 1 1 6 6 6 6 6 c . . 
+    . . c 6 1 1 1 1 1 7 6 6 c c . . 
+    . . . c c c c c c c c c c . . . 
+    `, SpriteKind.Player)
+tiles.placeOnRandomTile(mySprite2, sprites.dungeon.floorDarkDiamond)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.floorLight2)
+mySprite2.follow(mySprite, 50)
